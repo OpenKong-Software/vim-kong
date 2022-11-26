35883b41ec6a27737ec5126a6ae7de4939471583
SamuelWK <samuel@whitekongsoftware.com>
2022-11-26 15:44:21 +0100

    feat(plugin): add packer plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,27 +1,22 @@
-local install_path = vim.fn.stdpath('data') .. '~/.local/share/nvim/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  vim.fn.execute(
-    '!git clone https://github.com/wbthomason/packer.nvim ' .. install_path
-  )
+local vim = vim
+local execute = vim.api.nvim_command
+local fn = vim.fn
+-- ensure that packer is installed
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https://github.com/wbthomason/packer.nvim '..install_path)
+    execute 'packadd packer.nvim'
 end
-
-vim.cmd [[
-augroup Packer
-  autocmd!
-  autocmd BufWritePost init.lua PackerCompile
-augroup end
-]]
-
-use = require("packer").use
-require("packer").startup(function()
-  use 'neovim/nvim-lspconfig'
-
-  use {
-    'williamboman/nvim-lsp-installer',
-    requires = 'neovim/nvim-lspconfig',
-  }
-
-  use 'onsails/lspkind-nvim'
-  use {'tami5/lspsaga.nvim', requires = {'neovim/nvim-lspconfig'}}
-end)
+vim.cmd('packadd packer.nvim')
+local packer = require'packer'
+local util = require'packer.util'
+packer.init({
+  package_root = util.join_paths(vim.fn.stdpath('data'), 'site', 'pack')
+})
+--- startup and add configure plugins
+packer.startup(function()
+  local use = use
+  -- add you plugins here like:
+  -- use 'neovim/nvim-lspconfig'
+  end
+)
