cbff7bbe9b039e25ec84cf84c1c323fe0d564649
SamuelWK <samuel@whitekongsoftware.com>
2022-11-26 15:53:15 +0100

    feat(settings): add initial settings

---
+++ lua/settings.lua
@@ -0,0 +1,24 @@
+-- Show number in the gutter
+vim.opt.number = true
+
+-- Allows to use mouse
+vim.opt.mouse = 'a'
+
+-- Ignore uppercase on search
+vim.opt.ignorecase = true
+
+-- Search ignore uppercase letter unless the search has an uppercase letter
+vim.opt.smartcase = true
+
+-- Remove Higlights in the results of the previus search
+vim.opt.hlsearch = false
+
+-- Put this option in false if you want to make the text always visible
+vim.opt.wrap = true
+
+-- Tabs options
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = false
+
+
