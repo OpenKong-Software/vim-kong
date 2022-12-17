160e60286298b710cdbf3437118ad53ed1d6a57e
SamuelWK <samuel@whitekongsoftware.com>
2022-12-17 21:13:37 +0100

    fix(plugins): move setup to another file

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -18,6 +18,10 @@ packer.init({
 packer.startup(function()
   local use = use
 
+	use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+ 	  require("toggleterm").setup()
+	end}
+
   use {
     'nvim-neo-tree/neo-tree.nvim',
     branch = "v2.x",
@@ -32,51 +36,5 @@ packer.startup(function()
 end
 )
 
-require('neo-tree').setup({
-  close_if_last_window = true,
-  popup_border_style = 'rounded',
-  enable_git_status = true,
-  enable_diagnostics = true,
-  default_component_configs = {
-    indent = {
-      indent_size = 2,
-      padding = 1, -- extra padding on left hand side
-      with_markers = true,
-      indent_marker = '│',
-      last_indent_marker = '└',
-      highlight = 'NeoTreeIndentMarker',
-    },
-    icon = {
-      folder_closed = '',
-      folder_open = '',
-      default = '',
-    },
-  },
-  filesystem = {
-    follow_current_file = true,
-    use_libuv_file_watcher = true,
-    filtered_items = {
-      hide_dotfiles = true,
-    },
-    window = {
-      position = 'left',
-      mappings = {
-        f = 'none',
-      },
-    },
-  },
-})
+require("./plugins_setup")
 
-require("tokyonight").setup({
-  style = "night",
-  styles = {
-    functions = {}
-  },
-  sidebars = { "qf", "vista_kind", "terminal", "packer" },
-  on_colors = function(colors)
-    colors.hint = colors.orange
-    colors.error = "#ff0000"
-  end
-})
-
-vim.cmd [[colorscheme tokyonight]]
